a3de2a47a0fee2492259579b4ee08a8635d7083e
Pavel Koev <Pavkata7a@gmail.com>
2021-03-23 11:44:38 +0200

    add spring project

---
+++ TechnoMarket/src/main/java/techno_market/TechnoMarketSpringApplication.java
@@ -0,0 +1,13 @@
+package techno_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoMarketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnoMarketSpringApplication.class, args);
+    }
+
+}
